33e20deb5d293bf7efca4b3070bbf1dbef43d40e
Tejas Dalvi <74670868+Tejas-Dalvi-99@users.noreply.github.com>
2022-11-17 09:11:18 +0530

    Update Assignment1.sol

--- Assignment1.sol
+++ Assignment1.sol
@@ -2,11 +2,11 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Assignment{
-
+    // Question 1: Calculate cube of a given number
     function cubeOf(int a) public pure returns(int){
         return a*a*a ; 
     }
-
+    // Question 2: Check if given number is even or Odd
     function evenOdd(int b) public pure returns(int){
         if(b%2==0){
             return 1; 
@@ -15,7 +15,7 @@ contract Assignment{
             return 0; 
         }
     }
-    
+    // Question 3: Calculate Average of 3 Numbers
     function Avg(int a, int b, int c) public pure returns(int){
         return (a+b+c)/3; 
     }
